b569b52a00c302ee64e08ef574ed63bc086ff0f0
Joseph M <jmedina1985@hotmail.com>
2020-10-03 23:25:49 -0700

    Background color fade correction

--- 01_page_transition_tutorial/barba-css/src/js/app.js
+++ 01_page_transition_tutorial/barba-css/src/js/app.js
@@ -1,27 +1,35 @@
-import barba from "@barba/core";
-import barbaCss from "@barba/css";
+import barba from '@barba/core';
+import barbaCss from '@barba/css';
 
 // tell Barba to use the css plugin
 barba.use(barbaCss);
+
+const body = document.querySelector('body');
+
+// global hook running before any transitions
+barba.hooks.before((data) => {
+  const background = data.current.container.dataset.background;
+  body.style.setProperty('--page-background', background);
+});
 
 // init Barba - with the css plugin, once() hook will not run
 barba.init({
   transitions: [
     {
       beforeOnce() {
-        console.log("beforeOnce");
+        console.log('beforeOnce');
       },
       once() {
-        console.log("once");
+        console.log('once');
       },
       afterOnce() {
-        console.log("afterOnce");
+        console.log('afterOnce');
       },
     },
     {
-      name: "fade",
+      name: 'fade',
       to: {
-        namespace: ["fade"],
+        namespace: ['fade'],
       },
       leave() {},
       enter() {},
